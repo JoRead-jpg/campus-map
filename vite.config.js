28594903985a54b79b02745e920449126f595d94
dennies bor <borkiprono.com>
2024-03-20 22:44:08 -0400

    trying to fix jekyll problem

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import cesium from "vite-plugin-cesium";
+import cesium from "vite-plugin-cesium-build";
 
 // https://vitejs.dev/config/
 export default defineConfig({
